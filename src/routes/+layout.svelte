06d09a13c955c7dd09382f505fcdbddcfa48d603
Ralph4Win <kai.bleuel@hotmail.com>
2025-06-01 00:35:19 +0200

    My changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,37 @@
+<!-- src/routes/+layout.svelte -->
+
 <script>
-  import '/styles.css';
-  export let data;
+    import '../styles.css';
+    export let data;
 </script>
 
-<nav
-  class="navbar navbar-expand-lg bg-body-tertiary bg-dark"
-  data-bs-theme="dark"
->
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">GameStack</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/games">
-            Games
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/platforms">
-            Platforms
-          </a>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">GameStack</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/games">Games</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/platforms">Platforms</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
 
 <div class="container mt-3">
-  <slot />
+    <slot />
 </div>
